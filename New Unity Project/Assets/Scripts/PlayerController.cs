1c990ffd0dd38e9336b0b17e7edf79564251ba86
xennyisaguy <45329028+xennyisaguy@users.noreply.github.com>
2018-11-26 17:21:06 -0800

    Start Commit

---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody2D rb;
+
+    private float horizontalMovement;
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Move()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+            rb.AddForce(-transform.right * speed * Time.deltaTime);
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+
+            rb.AddForce(transform.right * speed * Time.deltaTime);
+            transform.localScale = new Vector3(1f, 1f, 1f);
+
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+
+
+
+        Move();
+
+    }
+
+}
---
+++ New Unity Project/Assets/Scripts/SceneMana.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMana : MonoBehaviour{
+
+    public string sceneName;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
