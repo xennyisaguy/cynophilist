cf0d8e6a7686413e88111bb555c0d6d8110903dd
DESKTOP-EIQ36Q5\andre <yerilth2@gmail.com>
2018-11-27 21:55:57 +1030

    added some stuff
    added a camera  you can try out some speeds it also has a pixel-perfect camera   so it won't look too weird moving
    I set it up so you can now use  unity built in title syteam for better level design  swapping out a box collider for a line collider  to save space

---
+++ New Unity Project/Assets/Scripts/cameraControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControl : MonoBehaviour {
+
+    public GameObject ThePlayer;
+    private Vector3 traget;
+[Tooltip("how fast the camera follows the player")]
+    public float cameraMoveSpeed;
+
+ 
+	
+    
+	// Update is called once per frame
+	void Update () {
+
+        traget = new Vector3(ThePlayer.transform.position.x, transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, traget, cameraMoveSpeed * Time.deltaTime);
+    }
+}
