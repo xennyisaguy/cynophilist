6c730c504e0cc96fa9b35c63dbddbe675194b4c2
DESKTOP-EIQ36Q5\andre <yerilth2@gmail.com>
2018-11-27 13:34:56 +1030

    fixed walking a bit
    hanged how walking works
    added some misisng animations

--- New Unity Project/Assets/Scripts/PlayerController.cs
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -4,46 +4,37 @@ using UnityEditor;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public float speed;
-
-    private Rigidbody2D rb;
-
-    private float horizontalMovement;
-
-    void Start()
+    [Tooltip("its very responsive now so try not to go to fast")]
+    public float PlayerSpeed;
+    public Rigidbody2D PlayerRigidbody;
+    public Animator animator;
+    private Vector3 Direction;
+    [Tooltip("its usefull for cutseens")]
+    public bool NotWalking;
+     void Start()
     {
 
-        rb = GetComponent<Rigidbody2D>();
 
     }
 
-    void Move()
-    {
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-
-            rb.AddForce(-transform.right * speed * Time.deltaTime);
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-
-            rb.AddForce(transform.right * speed * Time.deltaTime);
-            transform.localScale = new Vector3(1f, 1f, 1f);
-
-        }
-
-    }
+  
 
     void FixedUpdate()
     {
+        if(NotWalking == false)
+        {
+            Direction = Vector2.zero;
+            Direction.x = Input.GetAxisRaw("Horizontal");
+            Direction.y = transform.position.y;
+            if (Direction != Vector3.zero)
+            {
+                PlayerRigidbody.MovePosition(transform.position + Direction * PlayerSpeed * Time.deltaTime);
+                animator.SetFloat("Horizontal", Direction.x);
+ 
+            }
 
+        }
 
-
-        Move();
 
     }
 
