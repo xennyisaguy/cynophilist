571b18813c170fcc22afb0eb9083622e4deb6fe6
DESKTOP-EIQ36Q5\andre <yerilth2@gmail.com>
2018-11-28 17:09:52 +1030

    camra fixes
    not much just made the player and the camra prefabs

--- New Unity Project/Assets/Scripts/NextLev.cs
+++ New Unity Project/Assets/Scripts/NextLev.cs
@@ -5,12 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class NextLev : MonoBehaviour
 {
-
+    [Tooltip("the name of the next scene")]
+    public string SceneName;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            SceneManager.LoadScene("Scene2"); // loads next level
+            SceneManager.LoadScene(SceneName); 
         }
     }
 
--- New Unity Project/Assets/Scripts/cameraControl.cs
+++ New Unity Project/Assets/Scripts/cameraControl.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class cameraControl : MonoBehaviour {
 
-    public GameObject ThePlayer;
+    private GameObject ThePlayer;
     private Vector3 traget;
 [Tooltip("how fast the camera follows the player")]
     public float cameraMoveSpeed;
+    public void Start()
+    {
+        ThePlayer = GameObject.FindWithTag("Player");        
+    }
 
- 
-	
-    
-	// Update is called once per frame
-	void Update () {
+
+
+    // Update is called once per frame
+    void Update () {
 
         traget = new Vector3(ThePlayer.transform.position.x, transform.position.y, transform.position.z);
         transform.position = Vector3.Lerp(transform.position, traget, cameraMoveSpeed * Time.deltaTime);
